808c540b748ea856dc679e0783f063717258808b
George G. Vega Yon <g.vegayon@gmail.com>
2018-03-19 16:32:08 -0700

    Making progress on the formulas!

--- playground/formula.R
+++ playground/formula.R
@@ -4,27 +4,57 @@ data("medInnovationsDiffNet")
 
 diffnet_formula <- function(model) {
 
-  # Get the terms
-  ans  <- terms.formula(model)
+  # Checks ---------------------------------------------------------------------
+
+  # It must be a formula
+  if (!inherits(model, "formula"))
+    stop("Not a formula", call. = FALSE)
+
+  # Get the terms and checking whether exposure is present
+  terms <- terms.formula(model)
+  labs  <- attr(terms, "term.labels")
+  test  <- which(grepl("^exposure(\\(|\\+|\\s)", labs))
+
+  if (!length(test))
+    stop("No `exposure` term in the formula", call. = FALSE)
+
+  exposure_term <- labs[test]
+
 
   # Fetching the diffnet
-  obj <- eval(attr(ans, "variables")[[2]])
+  LHS <- attr(terms, "variables")[[2]]
 
-  if (!inherits(obj, "diffnet"))
+  if (!inherits(eval(LHS), "diffnet"))
     stop("The Left Hand Side of the formula is not a `diffnet` object.",
          call. = FALSE)
 
-  vars <- eval(attr(ans, "variables"))
+  diffnet_term <- labs[1L]
 
-  # is the first a diffnet object?
-  if (!inherits(vars[[1]], "diffnet"))
-    stop("Not an object of class diffnet.")
+  # Updating the formula -------------------------------------------------------
+  # Now the model will be Adopt ~ Exposure + ...
+  model_call <- update.formula(model, paste("~", exposure_term, "+."))
+  model <- update.formula(model, paste("Adopt ~ exposure + . -", exposure_term))
 
-  list(
-    1,
-    ans
-  )
+  # Computing exposure ---------------------------------------------------------
+  exposure_term <- attr(terms, "variables")[[test + 2L]]
+
+  # Replacing the network object
+  if (!is.call(exposure_term))
+    exposure_term <- as.call(list(exposure_term, graph = LHS))
+  else
+    exposure_term$graph <- LHS
+
+  exposure_term <- eval(exposure_term)
+
+  dat <- eval(LHS)
+  dat[["exposure"]] <- exposure_term
+  dat[["Adopt"]]    <- dat$cumadopt
+  dat <- as.data.frame(dat)
+
+  # Filtering the data ---------------------------------------------------------
+
+  glm(model, data=dat, family = gaussian(link = "logit"))
+
 }
 
-ans <- diffnet_formula(medInnovationsDiffNet ~ exposure(valued = TRUE))
-ans[[1]][[2]](ans[[1]][[1]])
+ans <- diffnet_formula(medInnovationsDiffNet ~ factor(city) + proage + exposure(valued = TRUE))
